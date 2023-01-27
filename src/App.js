911fe3a8b93762b63b3178751f0364c38bc9d8ee
lakshitha <sailakshitha409@gmail.com>
2023-01-27 18:58:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {data} from './components/data'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const list=data.map((car)=>{
+        return (
+          <div className='card-body' key={car.id}>
+              <img className="image" src={car.link}/>
+              <h2 className="heading">{car.name}</h2>
+              <div className="content">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut 
+                laoreet dolore magna aliquam erat volutpat.</div ><div className="body">
+              <button className="button">{car.content}</button>
+              </div>
+          </div>
+        )
+      }
+
+  )
+
+  return(
+    <div className="Card">
+      {list}
+      </div>
   );
 }
 
---
+++ src/components/data.js
@@ -0,0 +1,50 @@
+export const data=[
+    {
+        "id":1,
+        "name":"Mercedes-Benz",
+        "link":"https://cdn.carbuzz.com/gallery-images/1600/775000/200/775212.jpg",
+        "content":"more details"
+    },
+    {
+        "id":2,
+        "name":"Mclaren",
+        "link":"https://www.classicdriver.com/sites/default/files/cars_images/feed_681175/mclaren-p1-033.jpg",
+        "content":"more details"
+    },
+    {
+        "id":3,
+        "name":"Audi",
+        "link":"https://www.ultimatecarpage.com/images/car/6171/Audi-R8-e-tron-54673.jpg",
+        "content":"more details"
+    },
+    {
+        "id":4,
+        "name":"Ferrari",
+        "link":"https://th.bing.com/th/id/OIP.5XvBBo40UrFT_7ojdDWMywHaEK?pid=ImgDet&rs=1",
+        "content":'more details'
+    },
+    {
+        "id":5,
+        "name":"Porsche",
+        "link":"https://i1.wp.com/www.motoringresearch.com/wp-content/uploads/2018/06/Porsche_Taycan_03.jpg?resize=1500%2C1000&ssl=1",
+        "content":"more details"
+    },
+    {
+        "id":6,
+        "name":"Bugatti",
+        "link":"https://cdn.carbuzz.com/gallery-images/1600/460000/300/460392.jpg",
+        "content":"more details"
+    },
+    {
+        "id":7,
+        "name":"Lambhorgini",
+        "link":"https://th.bing.com/th/id/R.3c8f55aefd97b72c7f41cb7de3ad09cd?rik=OguU32D1iZWEpg&riu=http%3a%2f%2fwww.hdcarwallpapers.com%2fwalls%2f2016_print_tech_lamborghini_huracan-wide.jpg&ehk=97UTvzI2hqg8X1ZddXeTbRdxBQ6XceJ%2fIdMQ%2bGcekqM%3d&risl=&pid=ImgRaw&r=0",
+        "content":"more details"
+    },
+    {
+        "id":8,
+        "name":"Jaguar",
+        "link":"https://th.bing.com/th/id/OIP.yC5WLiK5T4JhQsJ8tVw1_AHaEo?pid=ImgDet&rs=1",
+        "content":"more details"
+    }
+]
